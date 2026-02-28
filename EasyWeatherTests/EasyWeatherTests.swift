2135793489defaaf84ffac50ed124f7adc857965
Ranel Tagirov <rntagirov@edu.hse.ru>
2026-02-28 14:24:55 +0300

    test: add unit and ui test targets

---
+++ EasyWeatherTests/EasyWeatherTests.swift
@@ -0,0 +1,17 @@
+//
+//  EasyWeatherTests.swift
+//  EasyWeatherTests
+//
+//  Created by Ranel Tagirov on 2/12/26.
+//
+
+import Testing
+@testable import EasyWeather
+
+struct EasyWeatherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
